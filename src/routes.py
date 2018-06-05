bcf1788c33b86b3373175ff8f6c9f0f7db95a168
Fredrik <fredrik.wallstrom.1992@gmail.com>
2018-06-05 16:19:19 +0200

    Start working on website

--- src/routes.py
+++ src/routes.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from src import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Testar livet"
+    user = {'username': 'Fredrik Wallström'}
+    return render_template('index.html', title='Home', user=user)
