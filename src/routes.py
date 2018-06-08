8ae576453c44d865e9112711d60f78647c384790
Fredrik <fredrik.wallstrom.1992@gmail.com>
2018-06-08 17:33:49 +0200

    New background image

--- src/routes.py
+++ src/routes.py
@@ -2,7 +2,6 @@ from flask import render_template
 from src import app
 
 @app.route('/')
-@app.route('/index')
-def index():
+def home():
     user = {'username': 'Fredrik Wallström'}
-    return render_template('index.html', title='Home', user=user)
+    return render_template('home.html', title='Home', user=user)
