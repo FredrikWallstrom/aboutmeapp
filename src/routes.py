2f477611cd712bfb57497bb9a878e2ef02ea9d4d
Fredrik <fredrik.wallstrom.1992@gmail.com>
2018-06-12 13:46:03 +0200

    Website now up and running, now time to modify it, change color scheme and so on

--- src/routes.py
+++ src/routes.py
@@ -1,10 +1,6 @@
 from flask import render_template
-from flask_bootstrap import Bootstrap
 from src import app
-
-Bootstrap(app)
 
 @app.route('/')
 def home():
-    user = {'username': 'Fredrik Wallström'}
     return render_template('index.html')
